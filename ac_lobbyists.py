5471aa50aa313c1c0b7a70f11d6f8c3c9f3fee30
AmyJo Brown <amyjobrown@gmail.com>
2013-06-10 18:06:51 -0400

    added header to code

--- ac_lobbyists.py
+++ ac_lobbyists.py
@@ -1,3 +1,6 @@
+# This code grabs a list of lobbyists registered in the city of Pittsburgh.
+# It only pulls in those with the last name "A" at this point. Need to edit code so that it pages 
+# through the site to get all the names.
 
 import urllib
 from bs4 import BeautifulSoup
